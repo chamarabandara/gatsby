1344a503d55591013e28461c0f24c906cea13f46
Sandaruwan Bandara <SandaruwanB@embla.lk>
2019-07-12 14:28:00 +0530

    init commit 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 
 export default () => (
-  <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+  <div style={{ color: `purple`, fontSize: `72px` }}>
+    Hello Gatsby! sandaruwan
+  </div>
 )
